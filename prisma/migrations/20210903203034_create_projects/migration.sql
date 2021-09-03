09fa38e79c24682f442580ff2e94f235a8516ded
ekateryna-rodina <work.rodina@gmail.com>
2021-09-03 16:40:30 -0400

    first migration is added

---
+++ prisma/migrations/20210903203034_create_projects/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Project" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "className" TEXT NOT NULL,
+    "logo" TEXT NOT NULL,
+
+    PRIMARY KEY ("id")
+);
